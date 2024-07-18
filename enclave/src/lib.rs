4a6edc5a648b195be54608d1a684a6cf2ebe6d3b
Till Ziegler <tz@schoeneweide.tk>
2024-07-18 17:56:53 +0200

    fix: align enclave with cosmos-lcp-demo

--- enclave/src/lib.rs
+++ enclave/src/lib.rs
@@ -1,6 +1,8 @@
 #![no_std]
 extern crate alloc;
+
 use enclave_runtime::{setup_runtime, Environment, MapLightClientRegistry};
+use ethereum_elc;
 
 setup_runtime!({
     simple_logger::SimpleLogger::new()
@@ -13,5 +15,6 @@ setup_runtime!({
 fn build_lc_registry() -> MapLightClientRegistry {
     let mut registry = MapLightClientRegistry::new();
     tendermint_lc::register_implementations(&mut registry);
+    ethereum_elc::register_implementations::<30,20>(&mut registry);
     registry
 }
